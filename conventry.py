d2ec55f95dee333849a94c3b2ee1b6754a9d8fb3
Ben Wing <ben@benwing.com>
2015-09-22 09:24:12 -0500

    conventry: More work, still not done or working

--- conventry.py
+++ conventry.py
@@ -6,6 +6,8 @@ import pywikibot, mwparserfromhell, re, string, sys, codecs, urllib2, datetime, 
 import blib
 from blib import getparam, rmparam
 
+import rulib as ru
+
 site = pywikibot.Site()
 save = False
 verbose = True
@@ -66,6 +68,8 @@ def process_page(index, page):
 
   for t in parsed.filter_templates():
     if unicode(t.name) == "ru-noun-table":
+
+      # Punt if multi-arg-set, can't handle yet
       should_continue = False
       for param in t.params:
         if unicode(param.value) == "or":
@@ -74,42 +78,78 @@ def process_page(index, page):
           break
       if should_continue:
         continue
-    stress = getparam(t, "1")
-    lemma = getparam(t, "2")
-    decl = getparam(t, "3")
-    bare = getparam(t, "4")
-    plstem = getparam(t, "5")
-    if not arg1_is_stress(stress):
-      assert not plstem
-    plstem = bare
-    bare = decl
-    decl = lemma
-    lemma = stress
-    stress = ""
-    newdecl = ""
-    decl, nsubs = re.subn(ur"(\d|[^/\w])ё(\d|[^/\w]|$)", r"\1\2", decl)
-    if nsubs:
-      newdecl += u";ё"
-    decl, nsubs = re.subn(r"\(1\)", "", decl)
-    if nsubs:
-      newdecl += "(1)"
-    decl, nsubs = re.subn(r"\(2\)", "", decl)
-    if nsubs:
-      newdecl += "(2)"
-    decl, nsubs = re.subn(r"\*", "", decl)
-    if nsubs:
-      newdecl += "*"
-    decl = re.sub(";", "", decl)
-    m = re.match(u"^([mfn]|3f|)(-ья)?", decl)
-    if m:
-      newdecl = decl + newdecl
-    elif "/" in decl:
-      pagemg("Found slash decl in template, not changing: %s" % unicode(t))
-      newdecl = decl + newdecl
-    else:
-      FIXME
 
-    FIXME
+      # Retrieve params; if first arg isn't stress, then it's lemma
+      # and shift everything down
+      stress = getparam(t, "1")
+      lemma = getparam(t, "2")
+      decl = getparam(t, "3")
+      bare = getparam(t, "4")
+      plstem = getparam(t, "5")
+      if not arg1_is_stress(stress):
+        assert not plstem
+        plstem = bare
+        bare = decl
+        decl = lemma
+        lemma = stress
+        stress = ""
+      if not lemma:
+        lemma = pagetitle
+
+      # Extract specials from decl, put into newdecl
+      newdecl = ""
+      decl, nsubs = re.subn(ur"(\d|[^/\w])ё(\d|[^/\w]|$)", r"\1\2", decl)
+      if nsubs:
+        newdecl += u";ё"
+      decl, nsubs = re.subn(r"\(1\)", "", decl)
+      if nsubs:
+        newdecl += "(1)"
+      decl, nsubs = re.subn(r"\(2\)", "", decl)
+      if nsubs:
+        newdecl += "(2)"
+      decl, nsubs = re.subn(r"\*", "", decl)
+      if nsubs:
+        newdecl += "*"
+      decl = re.sub(";", "", decl)
+
+      # If remaining decl is new format (opt. GENDER then opt. -ья), do nothing
+      m = re.match(u"^([mfn]|3f|)(-ья)?", decl)
+      if m:
+        newdecl = decl + newdecl
+      # If slash decl, also do nothing
+      elif "/" in decl:
+        pagemg("Found slash decl in template, not changing: %s" % unicode(t))
+        newdecl = decl + newdecl
+      # Else, single old-style decl; convert to new by transfering the ending
+      # to the end of the lemma and incorporating and new special marker if
+      # needed
+      else:
+        ending_stressed = ru.is_stressed(decl)
+        if decl != u"е́":
+          decl = ru.remove_accents(decl)
+        if decl not in conv_decl:
+          pagemsg("WARNING: Unrecognized declension %s in %s" %
+              (decl, unicode(t)))
+          continue
+        declconv = conv_decl[decl]
+        # FIXME: If declconv[0] is "-ин" we might not be able to transfer it
+        lemma = lemma + declconv[0]
+        if len(declconv) == 0:
+          special = []
+        elif type(declconv[1]) is list:
+          special = declconv[1]
+        else:
+          special = [declconv[1]]
+          for spec in ["m", "f", u"-ья", "(1)"]:
+            if spec in special:
+              if spec not in newdecl:
+                newdecl += spec
+              special = [x for x in special if x ~= spec]
+          assert not special
+
+        FIXME
+
+      FIXME
 
   if verbose:
     pagemsg("Replacing [[%s]] with [[%s]]" % text, newtext)
