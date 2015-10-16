5f15eeba2aad0aeaca6f535c356546ab081ceff7
Ben Wing <ben@benwing.com>
2015-10-16 18:28:24 -0500

    addpron: Bug fixes, now works

--- addpron.py
+++ addpron.py
@@ -6,6 +6,8 @@ import pywikibot, mwparserfromhell, re, string, sys, codecs, urllib2, datetime, 
 import blib
 from blib import getparam, rmparam
 
+import rulib as ru
+
 site = pywikibot.Site()
 save = False
 verbose = True
@@ -19,33 +21,35 @@ def errmsg(text):
 def process_page(index, page):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
-    msg("Page %s %s: %s" % (index, page, txt))
+    msg("Page %s %s: %s" % (index, pagetitle, txt))
 
   if not page.exists():
     pagemsg("WARNING: Page doesn't exist")
     return
 
   text = unicode(page.text)
-  parsed = blib.parse(text)
+  parsed = blib.parse_text(text)
 
   # Get the headword pronunciation(s)
   headword_pronuns = set()
   for t in parsed.filter_templates():
-    if unicode(t.name) == "ru-noun":
-      headword_pronuns.add(getparam(t, "1") or pagetitle)
+    if unicode(t.name) in ["ru-noun", "ru-proper noun", "ru-adj"]:
+      headword_pronuns.add(blib.remove_links(getparam(t, "1") or pagetitle))
       for i in xrange(2, 10):
-        headn = getparam(t, "head" + str(i))
+        headn = blib.remove_links(getparam(t, "head" + str(i)))
         if headn:
           headword_pronuns.add(headn)
       if getparam(t, "tr"):
         # FIXME, ru-IPA should take a tr parameter and use it in preference to
         # the Cyrillic
         pagemsg("WARNING: Don't know how to handle tr= param yet: %s" % unicode(t))
+        return
       for i in xrange(2, 10):
         trn = getparam(t, "tr" + str(i))
         if trn:
           pagemsg("WARNING: Don't know how to handle tr%s= param yet: %s" % (
             i, unicode(t)))
+          return
 
     elif unicode(t.name) == "ru-noun+":
       pagemsg("WARNING: Don't know how to handle ru-noun+ yet: %s" % unicode(t))
@@ -62,11 +66,14 @@ def process_page(index, page):
     # FIXME, should simply put multiple pronunciations in section
     return
   pronun = list(headword_pronuns)[0]
+  if ru.needs_accents(pronun):
+    pagemsg("WARNING: Pronunciation lacks accents, skipping: %s" % pronun)
+    return
 
   foundrussian = False
-  sections = re.split("(^==[^=]*==\n)", text, re.M)
+  sections = re.split("(^==[^=]*==\n)", text, 0, re.M)
   newtext = text
-  for j in xrange(1, len(sections), 2):
+  for j in xrange(2, len(sections), 2):
     if sections[j-1] == "==Russian==\n":
       if foundrussian:
         pagemsg("WARNING: Found multiple Russian sections")
@@ -82,16 +89,17 @@ def process_page(index, page):
       if re.search(r"^===+Pronunciation===+$", sections[j], re.M):
         pagemsg("WARNING: Found pronunciation section without ru-IPA")
         return
-      pronunsection = "===Pronunciation===\n{{ru-IPA|%s}}\n\n" % pronun
+      pronunsection = "===Pronunciation===\n* {{ru-IPA|%s}}\n\n" % pronun
       if re.search(r"^===Etymology [0-9]+===$", sections[j], re.M):
         pagemsg("WARNING: Found multiple etymology sections, can't handle yet")
         return
       if re.search(r"^===Etymology===$", sections[j], re.M):
-        newtext = re.sub(r"(^===Etymology===\n.*?\n)(==)", r"\1%s\2" % pronunsection, text, re.M, 1)
+        sections[j] = re.sub(r"(^===Etymology===\n.*?\n)(==)", r"\1%s\2" % pronunsection, sections[j], 1, re.M | re.S)
       elif re.search(r"^===Alternative forms===$", sections[j], re.M):
-        newtext = re.sub(r"(^===Alternative forms===\n.*?\n)(==)", r"\1%s\2" % pronunsection, text, re.M, 1)
+        sections[j] = re.sub(r"(^===Alternative forms===\n.*?\n)(==)", r"\1%s\2" % pronunsection, sections[j], 1, re.M | re.S)
       else:
-        newtext = re.sub(r"(^\n*)", r"\1%s" % pronunsection, text)
+        sections[j] = re.sub(r"(^\n*)", r"\1%s" % pronunsection, sections[j])
+      newtext = "".join(sections)
 
   if not foundrussian:
     pagemsg("WARNING: Can't find Russian section")
@@ -102,7 +110,7 @@ def process_page(index, page):
     return
 
   if verbose:
-    pagemsg("Replacing [[%s]] with [[%s]]" % text, newtext)
+    pagemsg("Replacing [[%s]] with [[%s]]" % (text, newtext))
 
   comment = "Add pronunciation %s" % pronun
   if save:
