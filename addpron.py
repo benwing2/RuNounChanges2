07270189a5f9ba98b6494441531a91348f95070e
Ben Wing <ben@benwing.com>
2015-11-14 13:26:17 -0600

    addpron: Handle ru-noun+, ru-proper noun+; don't use phon= with Latin translit; warn on use of Latin translit; check all lemmas and non-lemmas

--- addpron.py
+++ addpron.py
@@ -16,6 +16,9 @@ def msg(text):
 def errmsg(text):
   print >>sys.stderr, text.encode("utf-8")
 
+def contains_latin(text):
+  return re.search(u"[0-9a-zščžáéíóúýàèìòùỳɛ]", text.lower())
+
 def process_page(index, page, save, verbose):
   pagetitle = unicode(page.title())
   def pagemsg(txt):
@@ -24,6 +27,18 @@ def process_page(index, page, save, verbose):
   if not page.exists():
     pagemsg("WARNING: Page doesn't exist")
     return
+
+  def expand_text(tempcall):
+    if verbose:
+      pagemsg("Expanding text: %s" % tempcall)
+    result = site.expand_text(tempcall, title=pagetitle)
+    if verbose:
+      pagemsg("Raw result is %s" % result)
+    if result.startswith('<strong class="error">'):
+      result = re.sub("<.*?>", "", result)
+      pagemsg("WARNING: Got error: %s" % result)
+      return False
+    return result
 
   text = unicode(page.text)
   parsed = blib.parse_text(text)
@@ -35,28 +50,52 @@ def process_page(index, page, save, verbose):
     if unicode(t.name) in ["ru-noun", "ru-proper noun", "ru-adj", "ru-adv", "ru-verb", "ru-phrase"]:
       tr = getparam(t, "tr")
       if tr:
-        headword_pronuns.add("phon=" + tr)
+        pagemsg("WARNING: Using Latin for pronunciation, based on tr=%s" % (
+          tr))
+        headword_pronuns.add(tr)
       else:
         headword_pronuns.add(blib.remove_links(getparam(t, "1") or pagetitle))
       found_template = True
     elif unicode(t.name) == "head" and getparam(t, "1") == "ru":
       tr = getparam(t, "tr")
       if tr:
-        headword_pronuns.add("phon=" + tr)
+        pagemsg("WARNING: Using Latin for pronunciation, based on tr=%s" % (
+          tr))
+        headword_pronuns.add(tr)
       else:
         headword_pronuns.add(blib.remove_links(getparam(t, "head") or pagetitle))
       found_template = True
-    elif unicode(t.name) == "ru-noun+":
-      pagemsg("WARNING: Don't know how to handle ru-noun+ yet: %s" % unicode(t))
-      # FIXME, we should use ru-generate-form to extract the lemma; this
-      # means we need to create ru-generate-form and add "lemma" as a possible
-      # value
-      return
+    elif unicode(t.name) in ["ru-noun+", "ru-proper noun+"]:
+      if unicode(t.name) == "ru-noun+":
+        generate_template = re.sub(r"^\{\{ru-noun\+",
+            "{{ru-generate-noun-forms", unicode(t))
+      else:
+        generate_template = re.sub(r"^\{\{ru-proper noun\+",
+            "{{ru-generate-noun-forms|ndef=sg", unicode(t))
+      generate_result = expand_text(generate_template)
+      if not generate_result:
+        pagemsg("WARNING: Error generating noun forms")
+        return
+      args = {}
+      for arg in re.split(r"\|", generate_result):
+        name, value = re.split("=", arg)
+        args[name] = re.sub("<!>", "|", value)
+      lemma = args["nom_sg"] if "nom_sg" in args else args["nom_pl"]
+      for head in re.split(",", lemma):
+        if "//" in head:
+          _, tr = re.split("//", head)
+          pagemsg("WARNING: Using Latin for pronunciation, based on transit %s" % tr)
+          headword_pronuns.add(tr)
+        else:
+          headword_pronuns.add(head)
+
     if found_template:
       for i in xrange(2, 10):
         trn = getparam(t, "tr" + str(i))
         if trn:
-          headword_pronuns.add("phon=" + trn)
+          pagemsg("WARNING: Using Latin for pronunciation, based on tr%s=%s" % (
+            str(i), trn))
+          headword_pronuns.add(trn)
         else:
           headn = getparam(t, "head" + str(i))
           if headn:
@@ -66,10 +105,15 @@ def process_page(index, page, save, verbose):
     return
   headword_pronuns = sorted(list(headword_pronuns))
   pronun_lines = []
+  latin_char_msgs = []
   for pronun in headword_pronuns:
     if ru.needs_accents(pronun):
       pagemsg("WARNING: Pronunciation lacks accents, skipping: %s" % pronun)
       return
+    if contains_latin(pronun):
+      latin_char_msgs.append(
+          "WARNING: Pronunciation %s to be added contains Latin chars" %
+            pronun)
     pronun_lines.append("* {{ru-IPA|%s}}\n" % pronun)
 
   foundrussian = False
@@ -88,8 +132,13 @@ def process_page(index, page, save, verbose):
       foundpronuns = sorted(foundpronuns)
       if foundpronuns:
         if foundpronuns != headword_pronuns:
+          joined_foundpronuns = ",".join(foundpronuns)
+          joined_headword_pronuns = ",".join(headword_pronuns)
           pagemsg("WARNING: Existing pronunciation template has different pronunciation %s from headword-derived pronunciation %s" %
-                (",".join(foundpronuns), ",".join(headword_pronuns)))
+                (joined_foundpronuns, joined_headword_pronuns))
+          if "phon=" in joined_foundpronuns and not contains_latin(joined_headword_pronuns):
+            pagemsg("WARNING: Existing pronunciation template has pronunciation %s with phon=, headword-derived pronunciation %s isn't Latin, probably need manual translit" %
+                (joined_foundpronuns, joined_headword_pronuns))
         return
       if re.search(r"^===+Pronunciation===+$", sections[j], re.M):
         pagemsg("WARNING: Found pronunciation section without ru-IPA")
@@ -106,6 +155,9 @@ def process_page(index, page, save, verbose):
         sections[j] = re.sub(r"(^===)", r"%s\1" % pronunsection, sections[j], 1, re.M)
       sections[j] = re.sub("^===", "\n===", sections[j], 1)
       newtext = "".join(sections)
+      if latin_char_msgs:
+        for latinmsg in latin_char_msgs:
+          pagemsg(latinmsg)
 
   if not foundrussian:
     pagemsg("WARNING: Can't find Russian section")
@@ -127,7 +179,7 @@ def process_page(index, page, save, verbose):
     pagemsg("Would save with comment = %s" % comment)
 
 parser = argparse.ArgumentParser(description="Add pronunciation sections to Russian Wiktionary entries")
-parser.add_argument('pagefile', help="File containing pages to process, one per line")
+parser.add_argument('--pagefile', help="File containing pages to process, one per line")
 parser.add_argument('start', help="Starting page index", nargs="?")
 parser.add_argument('end', help="Ending page index", nargs="?")
 parser.add_argument('--save', action="store_true", help="Save results")
@@ -135,7 +187,14 @@ parser.add_argument('--verbose', action="store_true", help="More verbose output"
 args = parser.parse_args()
 start, end = blib.get_args(args.start, args.end)
 
-pages = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
-for i, page in blib.iter_items(pages, start, end):
-  msg("Page %s %s: Processing" % (i, page))
-  process_page(i, pywikibot.Page(site, page), args.save, args.verbose)
+if args.pagefile:
+  pages = [x.strip() for x in codecs.open(args.pagefile, "r", "utf-8")]
+  for i, page in blib.iter_items(pages, start, end):
+    msg("Page %s %s: Processing" % (i, page))
+    process_page(i, pywikibot.Page(site, page), args.save, args.verbose)
+else:
+  for category in ["Russian lemmas", "Russian non-lemma forms"]:
+    msg("Processing category: %s" % category)
+    for i, page in blib.cat_articles(category, start, end):
+      msg("Page %s %s: Processing" % (i, unicode(page.title())))
+      process_page(i, page, args.save, args.verbose)
